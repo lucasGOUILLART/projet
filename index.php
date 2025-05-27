25f7160735075607437584b63ab20316d3712b24
lucasGOUILLART <lucas.gouillart@student.junia.com>
2025-05-27 21:55:51 +0200

    index.php

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
+
+$host = $_ENV['PG_HOST'];
+$port = $_ENV['PG_PORT'];
+$db = $_ENV['PG_DB'];
+$user = $_ENV['PG_USER'];
+$password = $_ENV['PG_PASSWORD'];
+$endpoint = $_ENV['PG_ENDPOINT'];
+
+$connection_string = "host=" . $host . " port=" . $port . " dbname=" . $db . " user=" . $user . " password=" . $password . " options='endpoint=" . $endpoint . "' sslmode=require";
+
+$dbconn = pg_connect($connection_string);
+
+if (!$dbconn) {
+    die("Connection failed: " . pg_last_error());
+}
+echo "Connected successfully";
+
+<?php
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
